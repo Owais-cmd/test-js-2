07ddb5a3937a5f58bc6af0f4d5fe576c7a1c6f46
Owais Saud <owaistab18@gmail.com>
2026-06-04 05:18:03 +0000

    start

---
+++ impo.js
@@ -0,0 +1,7 @@
+
+
+export const controller = async (req, res) => {
+  // 2. Create a trace for this request
+  console.log("Creating trace..."
+  throw new Error("CRITICAL: Database connection lost during transaction!"
+}
---
+++ test.js
@@ -0,0 +1,29 @@
+import aida from 'aida-sdk'; // Use the new package name you published!
+import express from 'express'
+import { controller } from './impo.js';
+
+// 1. Initialize the SDK
+aida.init({
+  apiKey: "BBBjiojaof-aifsaoJJJoanfoainf",
+  projectId: "your-github-username/your-repo-name", 
+  baseUrl: "https://nondictatorially-spouseless-cornelius.ngrok-free.dev",
+  environment: "staging",
+  service: "my-test-script"
+});
+
+const app = express();
+
+app.get('/', async (req, res => {
+  
+    try {
+        await controller(req, re);
+    } catch (error) {
+        console.error("Error in controller:", error);
+        res.status500).send("Internal Server Error");
+    }
+  
+})
+
+app.listen(3001, () => {
+  console.log('Server is running on port 3001');
+});
