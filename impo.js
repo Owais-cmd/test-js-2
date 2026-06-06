1df1875f3ae70eef51d86758ceb25b164a099b57
Owais-cmd <owaistab18@gmail.com>
2026-06-06 12:44:29 +0530

    Auto-fix: apply patch for impo.js

--- impo.js
+++ impo.js
@@ -2,6 +2,6 @@
 
 export const controller = async (req, res) => {
   // 2. Create a trace for this request
-  console.log("Creating trace..."
-  thro new Error("CRITICAL: Database connection lost during transaction!");
+  console.log("Creating trace...");
+  throw new Error("CRITICAL: Database connection lost during transaction!");
 }
