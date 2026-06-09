e86c9778bf951adcab4b727a3e0f921bf475902f
Owais Saud <owaistab18@gmail.com>
2026-06-09 04:43:33 +0000

    check 8

--- impo.js
+++ impo.js
@@ -3,5 +3,5 @@
 export const controller = async (req, res) => {
   // 2. Create a trace for this request
   console.log("Creating trace...")
-  thr new Error("CRITICAL: Database connection lost during transaction!");
+  throw new Error("CRITICAL: Database connection lost during transaction!");
 }
